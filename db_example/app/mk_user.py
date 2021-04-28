82d9ff4e8a2d4a0bda34c6d88cad8c434d0abc17
marlobaguyos <marlo.baguyos@outlook.com>
2021-04-27 22:27:08 -0400

    update

---
+++ db_example/app/mk_user.py
@@ -0,0 +1,20 @@
+from app import db, User
+
+def create_my_user(first_name, last_name, hobbies):
+  """Simple user creation function"""
+  db.session.add(
+    User(
+      first_name=first_name,
+      last_name=last_name,
+      hobbies=hobbies
+      )
+    )
+  db.session.commit()
+
+if __name__ == "__main__":
+  create_my_user("Marlo", "Baguyos", "Movie")
+  user = User.query.all()
+  print(users)
+  create_my_user("John", "Doe", "Golfing")
+  user = User.query.filter_by(first_name="John").first()
+  print(user)
